4fd7038c84ba4e57a3a6677b9de761def726c422
QuanMD <quanmd@smartosc.com>
2023-06-05 15:19:34 +0700

    reaction feature

--- src/app.js
+++ src/app.js
@@ -12,6 +12,7 @@ var io = require('socket.io')(server, {
     allowEIO3: true
 });
 const port = 3000
+const messageReactions = {};
 
 app.use(express.static('src/public'));
 
@@ -20,10 +21,49 @@ app.get("/", function (req, res) {
 });
 
 io.on('connection', function (socket) {
-    socket.on('send', function (data) {
-        io.sockets.emit('send', data);
+    socket.on('send message', function (data) {
+        const messageId = (!data.id) ? generateMessageId() : data.id
+        let message = {}
+
+        if (!data.id) {
+            message = {
+                id: messageId,
+                message: data.message,
+                username: data.username,
+                reactions: [],
+            };
+            messageReactions[messageId] = message;
+        } else {
+            messageReactions[messageId]['message'] = data.message
+            message = messageReactions[messageId]
+            message.isEdit = true
+        }
+
+        io.sockets.emit('new message', message);
+    });
+
+    socket.on('react message', (data) => {
+        const { messageId, reaction } = data;
+
+        if (messageReactions.hasOwnProperty(messageId)) {
+            const message = messageReactions[messageId];
+
+            const existingReaction = message.reactions.find((r) => r.user === socket.id);
+
+            if (existingReaction) {
+                existingReaction.reaction = reaction;
+            } else {
+                message.reactions.push({ user: socket.id, reaction });
+            }
+
+            io.emit('update reactions', { messageId, reactions: message.reactions });
+        }
     });
 });
+
+function generateMessageId() {
+    return Date.now().toString() + Math.random().toString(36).substr(2, 5);
+}
 
 server.listen(port, () => {
     console.log(`Server is up on port ${port}`)
