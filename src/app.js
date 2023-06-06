12b7109adc294022e447dc3f0c4f473c38df6d32
QuanMD <quanmd@smartosc.com>
2023-06-06 17:38:49 +0700

    manage_account: login function

--- src/app.js
+++ src/app.js
@@ -1,24 +1,45 @@
-var path = require("path");
-var express = require("express");
-var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io')(server, {
+require('./db/mongoose')
+const express = require("express");
+const session = require('express-session');
+
+const app = express();
+const routeUser = require('./routers/user'),
+    routePage = require('./routers/page')
+const server = require('http').createServer(app);
+const port = process.env.PORT
+const io = require('socket.io')(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: `http://localhost:${port}`,
         methods: ["GET", "POST"],
         transports: ['websocket', 'polling'],
         credentials: true
     },
     allowEIO3: true
 });
-const port = 3000
 const messageReactions = {};
 
-app.use(express.static('src/public'));
+app.use(
+    session({
+        secret: 'app_chat_secret_key',
+        resave: false,
+        saveUninitialized: true,
+        cookie: {
+            secure: false,
+            maxAge: 3600000
+        },
+    })
+);
+app.use((req, res, next) => {
+    if (req.session && req.session.cookie.expires < Date.now()) {
+        console.log('Session expired:', req.sessionID);
+    }
 
-app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname + '/views/chat.html'));
+    next();
 });
+app.use(express.json())
+app.use(routeUser)
+app.use(routePage)
+app.use(express.static('src/public'));
 
 io.on('connection', function (socket) {
     socket.on('send message', function (data) {
---
+++ src/public/js/login.js
@@ -0,0 +1,28 @@
+$(function () {
+    $('#login-form').submit(function (event) {
+        event.preventDefault();
+
+        let username = $('#username').val(),
+            password = $('#password').val()
+
+        $.ajax({
+            type: 'POST',
+            url: '/user/login',
+            data: JSON.stringify({
+                username: username,
+                password: password
+            }),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function (response) {
+                console.log(response)
+                alert('Login successful!');
+                window.location.href = '/index'
+            },
+            error: function (xhr, status, error) {
+                console.log(xhr.responseText)
+                alert('Login failed. Please try again.');
+            }
+        });
+    })
+})
